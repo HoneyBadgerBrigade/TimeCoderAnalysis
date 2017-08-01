fa74d6818599a062df02db3566b94a991f3ff26f
Andrew Gaus <andrewgaus815@gmail.com>
2017-08-01 19:55:41 -0400

    Now everything with api is setup to write code for

--- JavaCodebase/src/main.java
+++ JavaCodebase/src/main.java
@@ -1,4 +1,22 @@
-import com.google.*;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.services.youtubereporting.YouTubeReporting;
+import com.google.api.services.youtubereporting.YouTubeReporting.Media.Download;
+import com.google.api.services.youtubereporting.model.Job;
+import com.google.api.services.youtubereporting.model.ListJobsResponse;
+import com.google.api.services.youtubereporting.model.ListReportsResponse;
+import com.google.api.services.youtubereporting.model.Report;
+
+import com.google.common.collect.Lists;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 public class main {
 
